74fb0b06e0923303c88d9f12deff2c3c02dbc9b2
goldenera-global <develop@goldenera.global>
2026-02-17 12:17:46 +0100

    feat: cosmetics

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,11 @@ import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import { deLocalizeUrl, localizeUrl } from './paraglide/runtime'
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
-import { queryClient } from './lib/react-query'
+import { QueryClient } from '@tanstack/react-query'
 
 // Create a new router instance
 export const getRouter = () => {
+  const queryClient = new QueryClient()
   const router = createRouter({
     routeTree,
     context: { queryClient, APP_NAME: 'GoldenEra Scan' },
