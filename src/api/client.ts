706de4be8b694d0058acd53b7b49028d8f2af187
goldenera-global <develop@goldenera.global>
2026-02-25 13:56:35 +0100

    feat: cosmetics

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 import fetch from '@kubb/plugin-client/clients/axios'
+import { createServerOnlyFn } from "@tanstack/react-start";
 
-export const getClient = () => {
-    const client = axios.create({
-        baseURL: process.env.NODE_API_URL,
-        headers: {
-            "Content-Type": "application/json",
-            "X-API-Key": process.env.NODE_API_KEY,
-            "User-Agent": "GE Explorer"
-        }
-    })
+const getNodeAuthParams = createServerOnlyFn(() => ({
+    baseURL: process.env.NODE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": process.env.NODE_API_KEY,
+        "User-Agent": "GE Explorer"
+    }
+}))
 
+
+export const getClient = createServerOnlyFn(() => {
+    const client = axios.create(
+        getNodeAuthParams()
+    )
     return client as unknown as typeof fetch
-};
+})
